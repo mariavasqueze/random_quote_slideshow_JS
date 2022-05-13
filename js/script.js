6c2caf6766508e43de02bcd2568db9e4314aceb9
Maria Vasquez <mariadelmarvze@gmail.com>
2022-05-13 14:26:20 -0700

    First commit

---
+++ js/script.js
@@ -0,0 +1,54 @@
+/******************************************
+*****************************************/
+
+/*** 
+ * `quotes` array 
+***/
+let quotes = [];
+
+
+/***
+ * `getRandomQuote` function
+***/
+function getRandomQuote() {
+    // 1. Create a variable that generates a random number
+ // between zero and the last index in the `quotes` array
+ // 2. Use the random number variable and bracket notation
+ // to grab a random object from the `quotes` array, and
+ // store it in a variable
+ // 3. Return the variable storing the random quote object
+}
+
+
+/***
+ * `printQuote` function
+***/
+function printQuote() {
+    // 1. Create a variable that calls the getRandomQuote()
+    // function
+    // 2. Create a variable that initiates your HTML string with
+    // the first two <p></p> elements, their classNames,
+    // and the quote and source properties, but leave off
+    // the second closing `</p>` tag for now
+    // 3. Use an if statement to check if the citation property
+    // exists, and if it does, concatenate a <span></span>
+    // element, appropriate className, and citation property
+    // to the HTML string
+    // 4. Use an if statement to check of the year property exists,
+    // and if it does, concatenate a <span></span> element,
+    // appropriate className, and year property to the HTML
+    //string
+    // 5. After the two if statements, concatenate the closing </p>
+    // tag to the HTML string
+    // 6. set the innerHTML of the quote-box div to equal the
+    // complete HTML string
+}
+
+
+/***
+ * click event listener for the print quote button
+ * The code will look like the following. You need to complete it.
+
+***/
+
+document.getElementById('load-quote').addEventListener // ...
